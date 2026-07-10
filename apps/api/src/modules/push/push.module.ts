cb0f7bb93277fef6dfd9add9ab5d94bb2f031881
Jefferson Moraes <jeffersonmoraes58@gmail.com>
2026-07-10 09:54:12 -0300

    fix: remove dependencia circular PushModule -> NotificationsModule

--- apps/api/src/modules/push/push.module.ts
+++ apps/api/src/modules/push/push.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PushController } from './push.controller';
 import { PushService } from './push.service';
-import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [NotificationsModule],
   controllers: [PushController],
   providers: [PushService],
   exports: [PushService],
